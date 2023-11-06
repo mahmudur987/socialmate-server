c9467f1454375ec2e13263b5525c163fae6fb0c9
mahmudur987 <safemahmud987@agmail.com>
2023-11-06 10:28:49 +0600

    Update postRoute.js

--- route/postRoute.js
+++ route/postRoute.js
@@ -12,12 +12,12 @@ const {
 } = require("../controller/postController");
 const verifyToken = require("../middleware/verifyToken");
 
-router.route("/post").post(verifyToken, postPost);
-router.route("/comment/:postId").post(postComment);
-router.route("/like/:postId").post(postLike);
-router.route("/updatepost/:id").patch(updatePost);
 router.route("/post").get(getPosts);
 router.route("/post/:postId").get(getPostById);
 router.route("/posts").get(getPostByEmail);
-router.route("/post/:id").delete(deletePost);
+router.route("/post").post(verifyToken, postPost);
+router.route("/comment/:postId").post(verifyToken, postComment);
+router.route("/like/:postId").post(verifyToken, postLike);
+router.route("/updatepost/:id").patch(verifyToken, updatePost);
+router.route("/post/:id").delete(verifyToken, deletePost);
 module.exports = router;
